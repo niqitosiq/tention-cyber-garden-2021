8aa8dc90cf7baf514c6385bd139fcdcf95728fa6
Ed Khalturin <DracVeg@gmail.com>
2021-12-11 01:25:36 +0300

    BACKEND: Подключил API paraphraser

--- backend/src/integrations/texttransformer.php
+++ backend/src/integrations/texttransformer.php
@@ -3,7 +3,33 @@
 namespace dvegasa\cg2021\integrations\texttransformer;
 
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+
 class TextTransformer {
+
+    protected function paraphraserCall (string $args): ResponseInterface {
+        $url = 'http://paraphraser.ru/api/';
+        $url .= '?token=' . $_ENV['PARAPHRASER_TOKEN'];
+        $url .= '&lang=ru';
+        $url .= '&format=json';
+        $url .= $args;
+
+        $client = new Client();
+        $response = $client->get($url);
+        return $response;
+    }
+
+    protected function ppWikitopic (array $words): array {
+        shuffle($words);
+        $args = '&query=' . implode(' ', $words);
+        $args .= '&c=wikitopic';
+        $response = $this->paraphraserCall($args);
+        $json = json_decode($response->getBody());
+        return $json?->response?->topics ?? array();
+    }
+
     // return string[] (size=n)
     function getSynonyms(string $word, int $n): array {
         $res = array();
@@ -15,6 +41,9 @@ class TextTransformer {
 
     // string[]
     function getCommonPhrase (array $words): string {
-        return 'Meow';
+        $variants = $this->ppWikitopic($words);
+        var_dump($variants);
+        shuffle($variants);
+        return $variants[0];
     }
 }
