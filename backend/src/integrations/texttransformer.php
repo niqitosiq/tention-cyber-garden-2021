c61347a70667c88705f4f624ab3889f927d7be25
Ed Khalturin <DracVeg@gmail.com>
2021-12-11 02:01:17 +0300

    BACKEND: Генерация общей фразы

--- backend/src/integrations/texttransformer.php
+++ backend/src/integrations/texttransformer.php
@@ -15,19 +15,29 @@ class TextTransformer {
         $url .= '&lang=ru';
         $url .= '&format=json';
         $url .= $args;
-
         $client = new Client();
         $response = $client->get($url);
         return $response;
     }
 
-    protected function ppWikitopic (array $words): array {
-        shuffle($words);
+    protected function ppWikitopics (array $words): array {
         $args = '&query=' . implode(' ', $words);
         $args .= '&c=wikitopic';
         $response = $this->paraphraserCall($args);
         $json = json_decode($response->getBody());
         return $json?->response?->topics ?? array();
+    }
+
+    protected function ppSynonyms (string $q): array {
+        $args = "&query=$q";
+        $args .= '&c=syns';
+        $args .= '&top=25';
+        $args .= '&forms=0';
+        $args .= '&scores=0';
+        $response = $this->paraphraserCall($args);
+        $json = json_decode($response->getBody(), true);
+        $res = $json['response'][1]['syns'];
+        return $res;
     }
 
     // return string[] (size=n)
@@ -41,9 +51,17 @@ class TextTransformer {
 
     // string[]
     function getCommonPhrase (array $words): string {
-        $variants = $this->ppWikitopic($words);
-        var_dump($variants);
-        shuffle($variants);
-        return $variants[0];
+        $topics = $this->ppWikitopics($words);
+        shuffle($topics);
+        $topicSynonyms = $this->ppSynonyms($topics[0]);
+        shuffle($topicSynonyms);
+//        var_dump(array(
+//                'words' => $words,
+//                'topic' => $topics,
+//                'topic[0]Synonyms' => $topicSynonyms,
+//        ));
+        $res = explode('|', $topicSynonyms[0])[0];
+        var_dump('getCommonPrase.res', $res);
+        return $res;
     }
 }
