e92d3dc4fbdb9373c629a2f802f604c625a21f5c
Ed Khalturin <DracVeg@gmail.com>
2021-12-10 20:39:07 +0300

    BACKEND: Загрузка .env

--- backend/src/main.php
+++ backend/src/main.php
@@ -2,6 +2,13 @@
 
 namespace dvegasa\cg2021\main;
 
+use Dotenv\Dotenv;
+
 function main (array $args): void {
+    loadEnvVars();
     echo 'Hello, world!';
+}
+
+function loadEnvVars() {
+    Dotenv::createImmutable(__DIR__ . '\..')->load();
 }
