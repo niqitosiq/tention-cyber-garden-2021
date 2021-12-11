954c8c67e13856a3d3fbd678592b4f66f1e8faab
Virg29 <256in4@gmail.com>
2021-12-11 22:58:35 +0300

    ahahahha pythooon blin

---
+++ kontur/gradientSelect.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+import cv2 as cv
+import numpy as np
+import base64
+import random
+import string
+import os
+import requests
+
+good_images = [];
+
+morph_size = 0
+max_operator = 2
+max_elem = 2
+max_kernel_size = 100
+title_trackbar_operator_type = 'Operator:\n 0: Opening - 1: Closing  \n 2: Gradient - 3: Top Hat \n 4: Black Hat'
+title_trackbar_element_type = 'Element:\n 0: Rect - 1: Cross - 2: Ellipse'
+title_trackbar_kernel_size = 'Kernel size:\n 2n + 1'
+trackbar_value = "Threshold"
+title_window = 'Morphology Transformations Demo'
+morph_op_dic = {0: cv.MORPH_GRADIENT, 1: cv.MORPH_TOPHAT, 2: cv.MORPH_BLACKHAT}
+
+
+def get_random_string(length):
+	# choose from all lowercase letter
+	letters = string.ascii_lowercase
+	result_str = ''.join(random.choice(letters) for i in range(length))
+	return result_str
+
+def writeImg(img,path):
+	cv.imwrite(path,img)
+
+
+def alphaBlendSrc2Dst(pathImg,pathMask):
+	img = cv.imread(pathImg)
+	mask = cv.imread(pathMask)
+	newShape = [min(img.shape[0],mask.shape[0]),min(img.shape[1],mask.shape[1])];
+
+	img = cv.resize(img,(newShape[1],newShape[0]))
+	mask = cv.resize(mask, (newShape[1], newShape[0]))
+	# img = img[0:newShape[0],0:newShape[1]]
+	# mask = mask[0:newShape[0],0:newShape[1]]
+	mask = 255 - mask
+	# blured = np.full(img.shape,255.0)
+	blured = cv.GaussianBlur(img, (19, 19), 11)
+
+	mask = cv.GaussianBlur(mask, (19, 19), 11)
+	# cv.invert(img,img)
+	if mask.ndim == 3 and mask.shape[-1] == 3:
+		alpha = mask/255.0
+	else:
+		alpha = cv.cvtColor(mask, cv.COLOR_GRAY2RGB)/255.0
+	blended = cv.convertScaleAbs(img*(1-alpha) + blured*alpha)
+	return blended
+
+def alphaBlend(img):
+	H,W = img.shape[:2]
+	mask = np.zeros((int(H),int(W)), np.uint8)
+	mask = cv.circle(mask, (int(W/2),int(H/2)), int(H/2.5), (255,255,255), -1, cv.LINE_AA)
+	mask = 255 - mask
+	mask = cv.GaussianBlur(mask, (21,21), 11)
+
+	blured = cv.GaussianBlur(img, (21,21), 11)
+
+	if mask.ndim==3 and mask.shape[-1] == 3:
+		alpha = mask/255.0
+	else:
+		alpha = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)/255.0
+	blended = cv.convertScaleAbs(img*(1-alpha) + blured*alpha)
+	return blended
+
+
+def gradientSelect(urls, write=False,pathInternalFile=None):
+	path = os.getcwd()+"/temp_grad/" + str(get_random_string(10)) + "."+urls[0].split(".")[-1]
+	pathW = os.getcwd()+"/generated/" + str(get_random_string(10)) + "."+urls[0].split(".")[-1]
+	src = None
+	if(pathInternalFile == None):
+		r = requests.get(urls[0], stream=True)
+		if r.status_code == 200:
+			with open(path, 'wb') as f:
+				for chunk in r:
+					f.write(chunk)
+		src = cv.imread(path)
+	else:
+		src = cv.imread(pathInternalFile)
+
+	cv.fastNlMeansDenoisingColored(src, src, 10, 14, 20, 20)
+
+	morph_operator = 1
+	morph_size = 14
+	# threshold_value = cv.getTrackbarPos(trackbar_value, title_window)
+
+	element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2*morph_size + 1, 2*morph_size+1), (morph_size, morph_size))
+	# elementEclipseSmall = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+	operation = morph_op_dic[morph_operator]
+	
+	
+
+	
+	
+	dst = alphaBlend(src)
+	dst = cv.morphologyEx(dst, operation, element)
+	# _, dst = cv.threshold(dst, threshold_value, 255, cv.THRESH_TRUNC )
+	dst = cv.cvtColor(dst,cv.COLOR_RGB2GRAY)
+
+
+
+
+	# dst = cv.medianBlur(dst,7)
+	# dst2 = cv.morphologyEx(dst, morph_op_dic[0], elementEclipseSmall)
+	if(write):
+		print(pathW)
+		cv.imwrite(pathW,dst)
+	retval, buffer = cv.imencode('.jpg', dst)
+	jpg_as_text = base64.b64encode(buffer).decode()
+	print(jpg_as_text[0:100])
+	return jpg_as_text
---
+++ kontur/lightMap.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import cv2 as cv
+import argparse
+import numpy as np
+import math
+import time
+import base64
+import random
+import string
+import os
+import requests
+
+
+def get_random_string(length):
+	# choose from all lowercase letter
+	letters = string.ascii_lowercase
+	result_str = ''.join(random.choice(letters) for i in range(length))
+	return result_str
+
+
+def lightMap(urls, write=False, pathInternalFile=None, rawImage = False):
+	path = os.getcwd()+"/temp_map/"+str(get_random_string(10)) + "." + urls[0].split(".")[-1]
+	pathW = os.getcwd()+"/generated/" + str(get_random_string(10)) + "." + urls[0].split(".")[-1]
+	src = None
+	if(pathInternalFile == None):
+		r = requests.get(urls[0], stream=True)
+		if r.status_code == 200:
+			with open(path, 'wb') as f:
+				for chunk in r:
+					f.write(chunk)
+		src = cv.imread(path)
+	else:
+		src = cv.imread(pathInternalFile)
+
+	# src = cv.resize(src, (0, 0), fx=0.5, fy=0.5)
+	src_gray = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+	just_white = np.zeros((src.shape[:2][0], src.shape[:2][1], 3), np.uint8)
+
+
+	divider = 30
+	array_of_contours = []
+	for i in range(math.floor(255/divider)):
+		threshold_type = 1
+		threshold_value = i*divider
+		print(threshold_value)
+		_, dst = cv.threshold(src_gray, threshold_value, 255, threshold_type )
+
+		
+		contours, hierarchy = cv.findContours(
+			dst, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+		sorted_list = list(sorted(contours, key=len))
+		# sorted_list = sorted_list[int(sortedlen*0.95):(sortedlen-1 if sortedlen!=0 else sortedlen)]
+		# sorted_list = [s for s in sorted_list if len(s)>400]
+		sorted_list = [s for s in sorted_list if cv.contourArea(s)>1200]
+		sortedlen = len(sorted_list)
+		jumpof_num = 10
+		flatten_filtered_contours = np.array([len(sorted_list)])
+		for contour in sorted_list:
+			contour_length = contour.shape[0]
+			if(contour_length > jumpof_num+1):
+				flatten_contour = np.array(contour.shape)
+				for j in range(contour_length):
+					if(j+jumpof_num >= contour_length):
+						break
+					contour[j][0][0] += (contour[j+jumpof_num][0][0] - contour[j][0][0])/2
+					contour[j][0][1] += (contour[j+jumpof_num][0][1] - contour[j][0][1])/2
+			
+
+
+		array_of_contours.append(sorted_list)
+	for counters in array_of_contours:
+		cv.drawContours(just_white, counters, -1, (255, 255, 255), 20)
+	if(write):
+		cv.imwrite(pathW, just_white)
+	os.remove(path)
+	retval, buffer = cv.imencode('.jpg', just_white)
+	jpg_as_text = base64.b64encode(buffer).decode()
+	return jpg_as_text
+
+
+
